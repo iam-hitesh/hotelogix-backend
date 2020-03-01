b213f73d4f6365051c265f64d9e4893e216bc702
Nidhi Sharma <nidhisharma199700@gmail.com>
2020-03-01 18:20:36 +0530

    change in model

--- models/Users/index.js
+++ models/Users/index.js
@@ -1,1 +1,16 @@
-User
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    phone_number: String,
+    isactive: Boolean,
+    profile_type: String
+  });
+
+
+  // Compile model from schema
+var UserModel = mongoose.model('UserModel', User );
